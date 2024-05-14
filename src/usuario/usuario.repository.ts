c1cb2d7c8bc688647681a6eae071256eb5ac9382
Matheus Kuaye Muramoto <matheusmuramoto@gmail.com>
2024-05-13 21:23:14 -0300

    first commit

---
+++ src/usuario/usuario.repository.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+export class UsuarioRepository {
+  private usuarios = [];
+
+  async salvar(usuario) {
+    this.usuarios.push(usuario);
+  }
+
+  async listar() {
+    return this.usuarios;
+  }
+}
